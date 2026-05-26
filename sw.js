ed76cc01c3609b958669e83bbb6058511b8c9d84
TausifSS <Tausifss015@gmail.com>
2026-05-26 19:54:27 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Simple pass-through service worker for HIVE PWA installation
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let the browser handle standard fetching
+});
